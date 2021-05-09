1e976af76ce047c268ade74edce141cc3a12a45d
Steve <60328295+therealsteveb@users.noreply.github.com>
2021-05-09 13:29:15 -0500

    PHP Email Template Update

--- modal/contact.php
+++ modal/contact.php
@@ -18,7 +18,7 @@ $php_email = filter_var($php_email, FILTER_SANITIZE_EMAIL);
 if (filter_var($php_email, FILTER_VALIDATE_EMAIL)) {
 
 
-	$php_subject = "Message from contact forum";
+	$php_subject = "Message from Stephen Bunel";
 
 	// To send HTML mail, the Content-type header must be set
 	$php_headers = 'MIME-Version: 1.0' . "\r\n";
@@ -27,13 +27,13 @@ if (filter_var($php_email, FILTER_VALIDATE_EMAIL)) {
 	$php_headers .= 'Cc:' . $php_email. "\r\n"; // Carbon copy to Sender
 
 	$php_template = '<div style="padding:20px;">Hello ' . $php_name . ',<br/>'
-	. 'Thank you for contacting us.<br/><br/>'
+	. 'Thank you for contacting me.<br/><br/>'
 	. '<strong style="color:#66B95C;">Name:</strong>  ' . $php_name . '<br/>'
 	. '<strong style="color:#66B95C;">Email:</strong>  ' . $php_email . '<br/>'
 	. '<strong style="color:#66B95C;">Message:</strong>  ' . $php_message . '<br/><br/>'
 	. 'This is a Contact Confirmation mail.'
 	. '<br/>'
-	. 'We will contact you as soon as possible .</div>';
+	. 'I will contact you as soon as possible .</div>';
 	$php_sendmessage = "<div style=\"background-color:#f5f5f5; color:#333;\">" . $php_template . "</div>";
 
 		// message lines should not exceed 70 characters (PHP rule), so wrap it
